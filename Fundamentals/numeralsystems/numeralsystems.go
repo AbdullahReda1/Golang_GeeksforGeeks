6debea85452f2695615ca0424a0f0b537ad7718c
AbdullahReda1 <abdullah.reda44@gmail.com>
2024-07-16 14:03:47 +0300

    Task 4

--- Fundamentals/numeralsystems/numeralsystems.go
+++ Fundamentals/numeralsystems/numeralsystems.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import "fmt"
 
+type point struct {
+	x bool
+	y bool
+}
+
+func main() {
+	print("dec:      ", 42, "\n")
+	fmt.Println("dec:\t ", 42)
+	fmt.Printf("dec:\t  %d\n", 42)
+
+	fmt.Printf("bin:\t  %b\n", 42)
+	fmt.Printf("bin:\t  %#b\n", 42)
+
+	fmt.Printf("hex:\t  %x\n", 42)
+	fmt.Printf("hex:\t  %#x\n", 42)
+
+	fmt.Printf("oct:\t  %o\n", 42)
+	fmt.Printf("oct:\t  %#o\n", 42)
+
+	//fmt.Printf("syn:\t  %+v\n", 42)
+
+	p := point{x: true, y: false}
+	fmt.Printf("bol:\t  %+v\n", p)
 }
