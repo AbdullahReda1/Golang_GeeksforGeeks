23711754c4bef2f6a2b798485191aa9f2c6e3a42
AbdullahReda1 <abdullah.reda44@gmail.com>
2024-07-23 02:15:04 +0300

    Task 6

--- Fundamentals/Gokeywords/Gokeywords.go
+++ Fundamentals/Gokeywords/Gokeywords.go
@@ -3,20 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	// Use of var to declare a variable
-	var msg string = "Hello world"
-	fmt.Printf(msg)
+	// Use of var to declare a variable: Declares a variable msg and assigns it a string value.
+	var msg string = "Hello, Go!"
+	fmt.Println(msg)
+
+	// Use of map: Declares a map `fruits` to store key-value pairs of strings.
+	fruits := map[string]string{
+		"a": "Apple",
+		"b": "Banana",
+		"c": "Cherry",
+	}
+	fmt.Println(fruits)
 }
 
 /*
-	qchan
-	qdefer
-	qfallthrough
-	qgo
-	qinterface
-	qmap
-	qrange
-	qselect
-	qtype
-	qvar
+	chan
+	defer
+	fallthrough
+	go
+	interface
+	map
+	range
+	select
+	type
 */
