77ec5da8f9efa538ff265d36164301bcd753bf0e
AbdullahReda1 <abdullah.reda44@gmail.com>
2024-07-24 19:36:10 +0300

    Task 6

--- Fundamentals/Gokeywords/Gokeywords.go
+++ Fundamentals/Gokeywords/Gokeywords.go
@@ -3,17 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	// Use of var to declare a variable: Declares a variable msg and assigns it a string value.
+	/* Use of var to declare a variable: Declares a variable msg and assigns it a string value */
 	var msg string = "Hello, Go!"
 	fmt.Println(msg)
-
-	// Use of map: Declares a map `fruits` to store key-value pairs of strings.
-	fruits := map[string]string{
-		"a": "Apple",
-		"b": "Banana",
-		"c": "Cherry",
-	}
-	fmt.Println(fruits)
 }
 
 /*
@@ -22,7 +14,6 @@ func main() {
 	fallthrough
 	go
 	interface
-	map
 	range
 	select
 	type
