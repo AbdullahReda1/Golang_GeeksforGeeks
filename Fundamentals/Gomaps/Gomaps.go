f91366f8502d4dbc4b2affd8514dee577cc5e966
AbdullahReda1 <abdullah.reda44@gmail.com>
2024-07-25 20:49:28 +0300

    Task 7

--- Fundamentals/Gomaps/Gomaps.go
+++ Fundamentals/Gomaps/Gomaps.go
@@ -9,22 +9,37 @@ func main() {
 	emptymap := map[int]string{}
 
 	// Using var keyword
-	var varmapresult bool
 	var varmap map[int]int
-	if varmap == nil {
-		varmapresult = true
-	} else {
-		varmapresult = false
-	}
 
 	// Valuable map
-	Valuablemap := map[string]string{
-		"a": "Apple",
-		"b": "Banana",
-		"c": "Cherry",
+	Valuablemap := map[int]string{
+		1: "Apple",
+		2: "Banana",
+		3: "Cherry",
 	}
 
-	fmt.Println(emptymap)
-	fmt.Println(varmap, " = ", varmapresult, "its nil")
+	// Using make function
+	var makemap = make(map[int]float32)
+	makemap[0] = 0.0
+	makemap[1] = 0.1
+	makemap[2] = 0.2
+	makemap[3] = 0.3
+	makemap[4] = 0.4
+	makemap[5] = 0.5
+
+	fmt.Println(emptymap, "It's an empty map ._.")
+	fmt.Println(varmap, "=", nil)
 	fmt.Println(Valuablemap)
+	fmt.Println(makemap)
+
+	// Iterate over map using for range "forr", The value of this loop may vary because the map is an unordered collection
+	for id, value := range makemap {
+		fmt.Println(id, "\t", value)
+	}
+
+	// Adding and changing key value pairs
+	makemap[6] = 0.6
+	makemap[0] = 0.01
+
+	fmt.Println(makemap)
 }
